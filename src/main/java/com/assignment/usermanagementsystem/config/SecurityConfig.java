9b1184080bb8ca75e8651ee932ad7cec097aaf47
unknown <skumaresan123@outlook.com>
2024-12-02 23:42:57 +0530

    CRUD done

--- src/main/java/com/assignment/usermanagementsystem/config/SecurityConfig.java
+++ src/main/java/com/assignment/usermanagementsystem/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                         .requestMatchers("/admin/**").hasAnyAuthority("ADMIN")
                         .requestMatchers("/user/**").hasAnyAuthority("USER")
                         .requestMatchers("/adminuser/**").hasAnyAuthority("ADMIN", "USER")
+                        .requestMatchers("/admin/employee/**").hasAnyAuthority("EMPLOYEE")
                         .anyRequest().authenticated())
                 .sessionManagement(manager->manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
